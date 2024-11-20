83763e5556bcf817a8b5c6a03b7324c6bb0c4371
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-20 12:02:25 +0200

    Move class to its own file

---
+++ classes.ts
@@ -0,0 +1,12 @@
+import { Review } from "./interfaces"
+
+export default class MainImage {
+    src: string
+    title: string
+    reviews: Review[]
+    constructor(src: string, title: string, reviews: Review[]) {
+        this.src = src
+        this.title = title
+        this.reviews = reviews
+    }
+}
