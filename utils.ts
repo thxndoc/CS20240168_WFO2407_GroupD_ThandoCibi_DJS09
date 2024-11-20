6c3e7d917b3b274f585720fe2cae73ceb414352d
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-20 08:27:52 +0200

    Practice adding void type

--- utils.ts
+++ utils.ts
@@ -1,10 +1,12 @@
+import { LoyaltyUser } from './enums'
+
 const reviewTotalDisplay = document.querySelector('#reviews')
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 
-export function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-    const iconDisplay = isLoyalty ? '⭐' : ''
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) : void {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = `review total ${value.toString()} | last reviewed by ${reviewer} ${iconDisplay}`
 }
 
 export function populateUser(isReturning : boolean, userName: string ) {
@@ -12,4 +14,14 @@ export function populateUser(isReturning : boolean, userName: string ) {
         returningUserDisplay.innerHTML = 'back'
     }
     userNameDisplay.innerHTML = userName
+}
+
+function add( firstValue: number, secondValue: number ) : number {
+    return firstValue + secondValue
+}
+
+export function makeMultiple(value: number) {
+    if (value > 1) {
+        return 's'
+    } 
 }
