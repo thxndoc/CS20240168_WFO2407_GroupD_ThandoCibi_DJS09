c8d705369b0f1d4de76e25a689fdba0b8e2f4a5d
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-20 10:40:36 +0200

    Create Property interface

--- interfaces.ts
+++ interfaces.ts
@@ -1,8 +1,23 @@
 import { LoyaltyUser } from "./enums";
+import { Price, Country } from "./types";
 
-export default interface Review {
+export interface Review {
     name: string;
     stars: number;
     loyaltyUser: LoyaltyUser;
     date: string;
+}
+
+export interface Property {
+    image: string;
+    title: string;
+    price: Price;
+    location: {
+        firstLine: string;
+        city: string;
+        postcode: number | string;
+        country: Country;
+    }
+    contact: [ number, string];
+    isAvailable: boolean;
 }
